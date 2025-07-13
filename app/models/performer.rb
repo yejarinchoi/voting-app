89fc010d901520c18cb837617887604cc5df9ca2
Yejee Arin Choi <yejee.choiii@gmail.com>
2025-07-12 21:03:18 -0700

    Make sure to implement has_many relationship

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -8,5 +8,6 @@
 #  updated_at :datetime         not null
 #
 class Performer < ApplicationRecord
+  has_many :voters,  dependent: :nullify # can delete a Peformer without deleting associated Voter
   validates :name, presence: true, uniqueness: true
 end
